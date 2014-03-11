8154ad3666db20cf0ef78402491a3be910ab066b
Jakub Cechacek <jcechace@redhat.com>
2014-03-11 13:45:28 +0100

    Property editor added -- it is now possible to add new properties in wizards

--- src/main/java/org/jboss/as/console/testsuite/fragments/ResourceTableFragment.java
+++ src/main/java/org/jboss/as/console/testsuite/fragments/ResourceTableFragment.java
@@ -17,23 +17,32 @@ public class ResourceTableFragment extends BaseFragment {
     // TODO: use properties (this is only temporary implementation)
 
     public ResourceTableRowFragment getRow(int index) {
-        // TODO: workaround - there is no cellTableRow class, thus odd and even row need to be selected separately
-        By selector = new ByJQuery("tr.cellTableEvenRow, tr.cellTableOddRow");
-        List<WebElement> rowElements = root.findElements(selector);
-        if (rowElements.isEmpty()) {
-            throw new NoSuchElementException("Cannot locate element using: " + selector);
-        }
+        List<WebElement> rowElements = getRowElements();
+
         WebElement rowRoot = rowElements.get(index);
 
-        ResourceTableRowFragment row = Graphene.createPageFragment(ResourceTableRowFragment.class, rowRoot);
+        ResourceTableRowFragment row = Graphene.createPageFragment(ResourceTableRowFragment.class,
+                rowRoot);
         return row;
     }
+
+    public ResourceTableRowFragment getLastRow() {
+        List<WebElement> rowElements = getRowElements();
+        WebElement rowRoot = rowElements.get(rowElements.size() -1);
+
+        ResourceTableRowFragment row = Graphene.createPageFragment(ResourceTableRowFragment.class,
+                rowRoot);
+        return row;
+    }
+
+
 
     public ResourceTableRowFragment getRowByText(int index, String text) {
         By selector = getRowByTextSelector(index, text);
         WebElement rowRoot = root.findElement(selector);
 
-        ResourceTableRowFragment row = Graphene.createPageFragment(ResourceTableRowFragment.class, rowRoot);
+        ResourceTableRowFragment row = Graphene.createPageFragment(ResourceTableRowFragment.class,
+                rowRoot);
         return row;
     }
 
@@ -47,15 +56,33 @@ public class ResourceTableFragment extends BaseFragment {
         return row;
     }
 
-    private static By getRowByTextSelector(int index, String text) {
+
+    private List<WebElement> getRowElements(boolean fail) {
+        // TODO: workaround - there is no cellTableRow class, thus odd and even row need to be selected separately
+        By selector = new ByJQuery("tr.cellTableEvenRow, tr.cellTableOddRow");
+        List<WebElement> rowElements = root.findElements(selector);
+
+        if (rowElements.isEmpty()) {
+            throw new NoSuchElementException("Cannot locate element using: " + selector);
+        }
+
+        return rowElements;
+    }
+
+    private List<WebElement> getRowElements() {
+        return getRowElements(true);
+    }
+
+    private By getRowByTextSelector(int index, String text) {
         // TODO: workaround - there is no cellTableRow class ...
         // TODO: find a better way than xpath
         // TODO: remove this method after resolving previous issues
-        String row = "tr[contains(@class, 'cellTableEvenRow') or contains(@class, 'cellTableOddRow')]";
+        String row = "tr[contains(@class, 'cellTableEvenRow') or " +
+                "contains(@class, 'cellTableOddRow')]";
         String cell = "td[contains(@class, 'cellTableCell')]";
         String containsText = "*[contains(text(), '" + text + "')]";
-        By selector = By.xpath(".//" + row + "//" + cell + "["+ (index + 1) + "]/descendant-or-self::" + containsText
-                                + "/ancestor::" + row);
+        By selector = By.xpath(".//" + row + "//" + cell + "["+ (index + 1) + "]" +
+                "/descendant-or-self::" + containsText + "/ancestor::" + row);
 
         return selector;
     }
