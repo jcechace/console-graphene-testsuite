a84383045695383244e7bb446bf1e5b932a16af5
Radim Hatlapatka <rhatlapa@redhat.com>
2014-03-20 16:36:54 +0100

    Patching tests refactored, to better manage state before and after test

--- src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/ApplyPatchTestCase.java
+++ src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/ApplyPatchTestCase.java
@@ -1,156 +1,74 @@
 package org.jboss.as.console.testsuite.tests.runtime.patching;
 
-import org.apache.commons.io.FileUtils;
-import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.console.testsuite.fragments.config.patching.PatchResultPanelFragment;
-import org.jboss.as.console.testsuite.fragments.shared.modals.WizardWindow;
-import org.jboss.as.console.testsuite.pages.runtime.PatchManagementPage;
 import org.jboss.as.console.testsuite.fragments.config.patching.PatchingWizard;
+import org.jboss.as.console.testsuite.pages.runtime.PatchManagementPage;
 import org.jboss.as.console.testsuite.util.Console;
 import org.jboss.as.console.testsuite.util.PropUtils;
-import org.jboss.qa.management.cli.CliClient;
-import org.jboss.qa.management.cli.PatchManager;
-import org.jboss.qa.management.common.Library;
 import org.jboss.qa.management.common.ServerUtils;
-import org.jboss.qa.management.common.ZipUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * @author rhatlapa (rhatlapa@redhat.com)
  */
 @RunWith(Arquillian.class)
-public class ApplyPatchTestCase {
-    @Drone
-    private WebDriver browser;
-
-    @Page
-    private PatchManagementPage patchManagementPage;
+public class ApplyPatchTestCase extends PatchTestCaseAbstract {
 
     private static final Logger log = LoggerFactory.getLogger(ApplyPatchTestCase.class);
 
-    private static CliClient cliClient = new CliClient();
-
-    private File testPatchFile;
-    private String patchName;
-
-
-    private PatchManager patchCliManager = new PatchManager(cliClient, true);
-
-    public ApplyPatchTestCase() {
-        String pathToPatchTemplate = getClass().getResource("/patching/console-test-patch-basic.zip.template").getPath();
-        testPatchFile = createZipWithUpdatedVersionInPatchXml(new File(pathToPatchTemplate));
-        patchName = "console-test-patch";
-    }
-
     @Before
-    public void setup() {
+    public void before() {
+        if (!ServerUtils.isServerRunning(cliClient)) {
+            ServerUtils.waitForServerToBecomeAvailable(cliClient);
+        }
+        cliClient.restart(false);
+        if (patchCliManager.isPatchInstaled(BASIC_PATCH_NAME)) {
+            removePatchViaCliUsingRollback(cliClient, BASIC_PATCH_NAME);
+        }
+
         Graphene.goTo(PatchManagementPage.class);
         Console.withBrowser(browser).waitUntilLoaded();
     }
 
     @After
     public void after() {
+        if (!ServerUtils.isServerRunning(cliClient)) {
+            ServerUtils.waitForServerToBecomeAvailable(cliClient);
+        }
+        if (patchCliManager.isPatchInstaled(BASIC_PATCH_NAME)) {
+            removePatchViaCliUsingRollback(cliClient, BASIC_PATCH_NAME);
+        }
+
         browser.navigate().refresh();
     }
 
-    @Test
-    public void rollbackPatch() {
-        try {
-            prepareAndApplyPatchViaCli(cliClient, testPatchFile, patchName);
-            log.debug("Patch applied => env prepared for rollback");
-            WizardWindow rollbackPatchWizard = patchManagementPage.rollbackPatch(patchName);
-            log.debug("Started rollback wizard");
-            rollbackPatchWizard.next();
-            rollbackPatchWizard.next();
-            log.debug("Finishing rollback wizard");
-            rollbackPatchWizard.finish();
-            log.debug("Wizard finished");
-            ServerUtils.waitForServerToBecomeAvailable(cliClient);
-            log.debug("Server should be started");
-            Assert.assertFalse("Patch rollback failed",
-                    patchCliManager.getPatchInfo().getCumulativePatchId().equals(patchName));
-            Assert.assertFalse("Restart is required by the server, should be already done by console",
-                    cliClient.restartRequired());
-        } catch (Throwable ex) {
-            log.error("Exception caught", ex);
-        } finally {
-            removePatchViaCliUsingRollback(cliClient, patchName);
-        }
-    }
 
     @Test
     public void applyValidPatch() {
-        try {
-            removePatchViaCliUsingRollback(cliClient, patchName);
-            applyPatch(testPatchFile);
-            Assert.assertTrue("Patch wasn't successfully applied, probably due https://bugzilla.redhat.com/show_bug.cgi?id=1074623",
-                    patchCliManager.getPatchInfo().getCumulativePatchId().equals(patchName));
-        } finally {
-            patchCliManager.rollback(patchName, false);
-        }
-    }
-
-    public void prepareAndApplyPatchViaCli(CliClient cliClient, File patch, String patchName) {
-        ServerUtils.waitForServerToBecomeAvailable(cliClient);
-        cliClient.restart(false);
-        if (!patchCliManager.isPatchInstaled(patchName)) {
-            patchCliManager.apply(testPatchFile.getAbsolutePath());
-        }
-        log.debug("patch {} applied", patchName);
-        Assert.assertTrue("Patch wasn't successfully applied via CLI",
-                patchCliManager.getPatchInfo().getCumulativePatchId().equals(patchName));
+        applyPatch(basicPatchFile, true);
+        Assert.assertTrue("Patch wasn't successfully applied, probably due https://bugzilla.redhat.com/show_bug.cgi?id=1074623",
+                patchCliManager.getPatchInfo().getCumulativePatchId().equals(BASIC_PATCH_NAME));
         browser.navigate().refresh();
     }
 
-    public void removePatchViaCliUsingRollback(CliClient cliClient, String patchName) {
-        ServerUtils.waitForServerToBecomeAvailable(cliClient);
-        cliClient.restart(false);
-        patchCliManager.rollback(patchName, false);
-    }
 
-
-    public void applyPatch(File patchFile) {
+    public void applyPatch(File patchFile, boolean expectedResult) {
         PatchingWizard applyPatchWizard = patchManagementPage.applyNewPatch();
         applyPatchWizard.getEditor().uploadFile(patchFile, PropUtils.get("runtime.patching.apply.fileupload.name"));
-        applyPatchWizard.next(); // only confirmation
         applyPatchWizard.next();
-        PatchResultPanelFragment errorPanel = applyPatchWizard.getResultPanel();
-        errorPanel.showErrorDetails();
-        Assert.assertTrue("Apply patch failed with " + errorPanel.getErrorMessage()
-                + ",\n error details:" + errorPanel.getErrorDetails(), false);
+        Assert.assertEquals("Result of applying patch, " + applyPatchWizard.getResultMessage(), expectedResult, applyPatchWizard.isSuccess());
 
+        applyPatchWizard.finish();
     }
 
-    public static File createZipWithUpdatedVersionInPatchXml(File srcPatchZip) {
-        try {
-            File newPatchZip = File.createTempFile("newPatch", ".zip");
-            newPatchZip.delete();
-            FileUtils.copyFile(srcPatchZip, newPatchZip);
-            File patchXml = File.createTempFile("patch", ".xml");
-            patchXml.delete();
-            if (!ZipUtils.extractOneFileFromZip(newPatchZip, "patch.xml", patchXml)) {
-                throw new RuntimeException("patch.xml not found in " + newPatchZip.getAbsolutePath());
-            }
-            String serverVersion = ServerUtils.getVersion(cliClient);
-            Library.replaceStringInFile("version=\"%EAP_VERSION%\"", "version=\""+serverVersion+"\"", patchXml);
-            ZipUtils.updateOneFileInZip(newPatchZip, patchXml, "patch.xml");
-            return newPatchZip;
-        } catch (IOException ioEx) {
-            ioEx.printStackTrace();
-            return null;
-        }
-    }
+
 }
---
+++ src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/PatchTestCaseAbstract.java
@@ -0,0 +1,85 @@
+package org.jboss.as.console.testsuite.tests.runtime.patching;
+
+import org.apache.commons.io.FileUtils;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.console.testsuite.pages.runtime.PatchManagementPage;
+import org.jboss.qa.management.cli.CliClient;
+import org.jboss.qa.management.cli.PatchManager;
+import org.jboss.qa.management.common.Library;
+import org.jboss.qa.management.common.ServerUtils;
+import org.jboss.qa.management.common.ZipUtils;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author rhatlapa (rhatlapa@redhat.com)
+ */
+@RunWith(Arquillian.class)
+public abstract class PatchTestCaseAbstract {
+
+    public static final String BASIC_PATCH_ZIP_TEMPLATE_NAME = "console-test-patch-basic.zip.template";
+    public static final String CUMULATIVE_PATCH_ZIP_TEMPLATE_NAME = "console-test-patch-cumulated.zip.template";
+
+    public static final String BASIC_PATCH_NAME = "basic-console-test-patch";
+    protected File basicPatchFile = initPatchFile(PatchTestCaseAbstract.BASIC_PATCH_ZIP_TEMPLATE_NAME, BASIC_PATCH_NAME);
+
+    @Drone
+    protected WebDriver browser;
+
+    @Page
+    protected PatchManagementPage patchManagementPage;
+
+    private static final Logger log = LoggerFactory.getLogger(PatchTestCaseAbstract.class);
+
+    protected static CliClient cliClient = new CliClient();
+
+    protected PatchManager patchCliManager = new PatchManager(cliClient, true);
+
+    public File initPatchFile(String patchFileTemplateName, String patchId) {
+        String pathToPatchTemplate = getClass().getResource("/patching/"+patchFileTemplateName).getPath();
+        return createPatchZipFromTemplate(new File(pathToPatchTemplate), ServerUtils.getVersion(cliClient), patchId);
+    }
+
+
+    public void prepareAndApplyPatchViaCli(CliClient cliClient, File patchFile, String patchName) {
+        patchCliManager.apply(patchFile.getAbsolutePath());
+        log.debug("patch {} applied", patchName);
+        Assert.assertTrue("Patch wasn't successfully applied via CLI",
+                patchCliManager.getPatchInfo().getCumulativePatchId().equals(patchName));
+        browser.navigate().refresh();
+    }
+
+    public void removePatchViaCliUsingRollback(CliClient cliClient, String patchName) {
+        patchCliManager.rollback(patchName, false);
+    }
+
+
+
+    public static File createPatchZipFromTemplate(File srcPatchZip, String patchServerVersion, String patchName) {
+        try {
+            File newPatchZip = File.createTempFile("newPatch", ".zip");
+            newPatchZip.delete();
+            FileUtils.copyFile(srcPatchZip, newPatchZip);
+            File patchXml = File.createTempFile("patch", ".xml");
+            patchXml.delete();
+            if (!ZipUtils.extractOneFileFromZip(newPatchZip, "patch.xml", patchXml)) {
+                throw new RuntimeException("patch.xml not found in " + newPatchZip.getAbsolutePath());
+            }
+            Library.replaceStringInFile("%EAP_VERSION%", patchServerVersion, patchXml);
+            Library.replaceStringInFile("%PATCH_NAME%", patchName, patchXml);
+            ZipUtils.updateOneFileInZip(newPatchZip, patchXml, "patch.xml");
+            return newPatchZip;
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+            return null;
+        }
+    }
+}
