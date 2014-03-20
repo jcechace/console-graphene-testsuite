c1382c8d5e2629d40ed18700ddecfb0509b70f27
Radim Hatlapatka <rhatlapa@redhat.com>
2014-03-20 11:28:53 +0100

    Refactoring => patching fragments moved to proper packages

--- src/main/java/org/jboss/as/console/testsuite/fragments/config/patching/PatchingWizard.java
+++ src/main/java/org/jboss/as/console/testsuite/fragments/config/patching/PatchingWizard.java
@@ -1,13 +1,12 @@
-package org.jboss.as.console.testsuite.pages.runtime;
+package org.jboss.as.console.testsuite.fragments.config.patching;
 
 import org.jboss.arquillian.graphene.Graphene;
-import org.jboss.as.console.testsuite.fragments.PatchErrorPanelFragment;
 import org.jboss.as.console.testsuite.fragments.shared.modals.WizardWindow;
 
 /**
  * @author rhatlapa (rhatlapa@redhat.com)
  */
-public class PatchingWizardWindow extends WizardWindow {
+public class PatchingWizard extends WizardWindow {
     public PatchErrorPanelFragment getErrorPanel() {
         return Graphene.createPageFragment(PatchErrorPanelFragment.class, root.findElement(PatchErrorPanelFragment.SELECTOR));
     }
