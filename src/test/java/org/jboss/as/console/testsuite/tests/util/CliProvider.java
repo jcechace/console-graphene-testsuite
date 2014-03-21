984958ab5d883f8383caea26487ef0516bb5f6c6
Jakub Cechacek <jcechace@redhat.com>
2014-03-21 18:25:16 +0100

    CliProvider class.

---
+++ src/test/java/org/jboss/as/console/testsuite/tests/util/CliProvider.java
@@ -0,0 +1,31 @@
+package org.jboss.as.console.testsuite.tests.util;
+
+import org.jboss.qa.management.cli.CliClient;
+import org.jboss.qa.management.cli.DomainCliClient;
+
+/**
+ * @author jcechace
+ */
+public class CliProvider {
+
+    private CliProvider() {
+
+    }
+
+    public  static CliClient withProfile(String profile) {
+        return new DomainCliClient(profile);
+    }
+
+    public static CliClient getClient() {
+        CliClient cliClient = null;
+        if (ConfigUtils.isDomain()) {
+            String profile = ConfigUtils.getDefaultProfile();
+            cliClient = new DomainCliClient(profile);
+        } else {
+            cliClient = new CliClient();
+        }
+
+        return cliClient;
+    }
+
+}
