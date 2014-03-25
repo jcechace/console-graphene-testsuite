3a6347e88bcd8c0daba799fa105596d76b67ed98
Jakub Cechacek <jcechace@redhat.com>
2014-03-25 14:40:00 +0100

    Homepage sidebar abstraction.

---
+++ src/main/java/org/jboss/as/console/testsuite/fragments/homepage/HomepageSideBarFragment.java
@@ -0,0 +1,46 @@
+package org.jboss.as.console.testsuite.fragments.homepage;
+
+import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.as.console.testsuite.fragments.BaseFragment;
+import org.jboss.as.console.testsuite.util.PropUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jcechace
+ */
+public class HomepageSideBarFragment extends BaseFragment {
+
+    public List<WebElement> getAllSectionElements() {
+        By selector = By.className(PropUtils.get("homepage.sidebar.section.class"));
+        List<WebElement> elements = root.findElements(selector);
+
+        return elements;
+    }
+
+    public List<HomepageSideSectionFragment> getAllSections() {
+        List<WebElement> elements = getAllSectionElements();
+        List<HomepageSideSectionFragment> sections = new ArrayList<HomepageSideSectionFragment>();
+
+        for (WebElement elem : elements)
+        {
+            sections.add(Graphene.createPageFragment(HomepageSideSectionFragment.class, elem));
+        }
+
+        return sections;
+    }
+
+    public HomepageSideSectionFragment getSection(String headerLabel) {
+        for (HomepageSideSectionFragment section : getAllSections()) {
+            if (section.getHeaderLabel().equalsIgnoreCase(headerLabel)) {
+                return section;
+            }
+        }
+        throw new NoSuchElementException("Unable to found sidebar section labeled: " + headerLabel);
+    }
+
+}
