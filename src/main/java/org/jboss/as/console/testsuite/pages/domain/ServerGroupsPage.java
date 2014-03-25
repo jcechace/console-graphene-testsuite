86ac5af6a15edbfc13683ea6b10db4e7030c41b2
Jakub Cechacek <jcechace@redhat.com>
2014-03-25 12:33:44 +0100

    Homepage abstraction and tests (entry point, sections).

---
+++ src/main/java/org/jboss/as/console/testsuite/pages/domain/ServerGroupsPage.java
@@ -0,0 +1,11 @@
+package org.jboss.as.console.testsuite.pages.domain;
+
+import org.jboss.arquillian.graphene.page.Location;
+import org.jboss.as.console.testsuite.pages.BasePage;
+
+/**
+ * @author jcechace
+ */
+@Location("#server-groups")
+public class ServerGroupsPage extends BasePage{
+}
---
+++ src/main/java/org/jboss/as/console/testsuite/pages/runtime/DomainDeploymentPage.java
@@ -0,0 +1,11 @@
+package org.jboss.as.console.testsuite.pages.runtime;
+
+import org.jboss.arquillian.graphene.page.Location;
+import org.jboss.as.console.testsuite.pages.BasePage;
+
+/**
+ * @author jcechace
+ */
+@Location("#domain-deployments")
+public class DomainDeploymentPage extends BasePage{
+}
