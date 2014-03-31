64fe304bdee3686ae8cd9bdb11a05cab0197e9c9
Radim Hatlapatka <rhatlapa@redhat.com>
2014-03-31 14:26:53 +0200

    Basic tests for patching via web console
    apply/rollback patch - cumulative patches + one-off patches

--- src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/ApplyPatchTestCase.java
+++ src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/ApplyPatchTestCase.java
@@ -1,9 +1,11 @@
 package org.jboss.as.console.testsuite.tests.runtime.patching;
 
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.console.testsuite.fragments.config.patching.PatchingWizard;
 import org.jboss.as.console.testsuite.pages.runtime.PatchManagementPage;
+import org.jboss.as.console.testsuite.tests.categories.StandaloneTest;
 import org.jboss.as.console.testsuite.util.Console;
 import org.jboss.as.console.testsuite.util.PropUtils;
 import org.jboss.qa.management.common.ServerUtils;
@@ -11,6 +13,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +24,8 @@ import java.io.File;
  * @author rhatlapa (rhatlapa@redhat.com)
  */
 @RunWith(Arquillian.class)
+@RunAsClient
+@Category(StandaloneTest.class)
 public class ApplyPatchTestCase extends PatchTestCaseAbstract {
 
     private static final Logger log = LoggerFactory.getLogger(ApplyPatchTestCase.class);
@@ -31,6 +36,9 @@ public class ApplyPatchTestCase extends PatchTestCaseAbstract {
             ServerUtils.waitForServerToBecomeAvailable(cliClient);
         }
         cliClient.restart(false);
+        if (patchCliManager.isPatchInstaled(CUMULATIVE_PATCH_NAME)) {
+            removePatchViaCliUsingRollback(cliClient, CUMULATIVE_PATCH_NAME);
+        }
         if (patchCliManager.isPatchInstaled(BASIC_PATCH_NAME)) {
             removePatchViaCliUsingRollback(cliClient, BASIC_PATCH_NAME);
         }
@@ -44,6 +52,9 @@ public class ApplyPatchTestCase extends PatchTestCaseAbstract {
         if (!ServerUtils.isServerRunning(cliClient)) {
             ServerUtils.waitForServerToBecomeAvailable(cliClient);
         }
+        if (patchCliManager.isPatchInstaled(CUMULATIVE_PATCH_NAME)) {
+            removePatchViaCliUsingRollback(cliClient, CUMULATIVE_PATCH_NAME);
+        }
         if (patchCliManager.isPatchInstaled(BASIC_PATCH_NAME)) {
             removePatchViaCliUsingRollback(cliClient, BASIC_PATCH_NAME);
         }
@@ -51,23 +62,49 @@ public class ApplyPatchTestCase extends PatchTestCaseAbstract {
         browser.navigate().refresh();
     }
 
+    @Test
+    public void applyValidPatchTest() {
+        applyPatch(basicPatchFile, BASIC_PATCH_NAME, true);
+        applyPatch(cumulativePatchFile, CUMULATIVE_PATCH_NAME, true);
+    }
 
     @Test
-    public void applyValidPatch() {
-        applyPatch(basicPatchFile, true);
-        Assert.assertTrue("Patch wasn't successfully applied, probably due https://bugzilla.redhat.com/show_bug.cgi?id=1074623",
-                patchCliManager.getPatchInfo().getCumulativePatchId().equals(BASIC_PATCH_NAME));
-        browser.navigate().refresh();
+    public void applyInvalidPatchTest() {
+        applyPatch(cumulativePatchFile, CUMULATIVE_PATCH_NAME, false); // requires basic patch to be installed, which is not in this state
+    }
+
+    @Test
+    public void applyOneOffPatchTest() {
+        final String oneOffPatchName = "dummyOneOffPatch";
+        File oneOffPatchSimple = initPatchFile("console-test-oneoff-patch.zip.template", oneOffPatchName);
+        try {
+            applyPatch(oneOffPatchSimple, oneOffPatchName, true);
+            Assert.assertEquals("Displayed patch type doesn't match", "one-off",
+                    patchManagementPage.getResourceTable().getRowByText(0, oneOffPatchName).getCellValue(2));
+        } finally {
+            if (patchCliManager.isPatchInstaled(oneOffPatchName)) {
+                removePatchViaCliUsingRollback(cliClient, oneOffPatchName);
+            }
+        }
     }
 
 
-    public void applyPatch(File patchFile, boolean expectedResult) {
+    private void applyPatch(File patchFile, String patchName, boolean expectedResult) {
         PatchingWizard applyPatchWizard = patchManagementPage.applyNewPatch();
         applyPatchWizard.getEditor().uploadFile(patchFile, PropUtils.get("runtime.patching.apply.fileupload.name"));
         applyPatchWizard.next();
-        Assert.assertEquals("Result of applying patch, " + applyPatchWizard.getResultMessage(), expectedResult, applyPatchWizard.isSuccess());
+        Assert.assertEquals("Result of applying patch, " + applyPatchWizard.getResultMessage(), expectedResult,
+                applyPatchWizard.isSuccess());
+        if (expectedResult) {
+            applyPatchWizard.finish();
+        } else {
+            applyPatchWizard.cancel();
+        }
+        Assert.assertEquals(patchName +" was successfully applied:", expectedResult, patchCliManager.isPatchInstaled(patchName));
+        Assert.assertFalse("Restart is required by the server, should be already done by console",
+                cliClient.restartRequired());
 
-        applyPatchWizard.finish();
+        Assert.assertEquals(patchName + " is visible in console:", expectedResult, patchManagementPage.getResourceTable().getRowByText(0, patchName) != null);
     }
 
 
--- src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/RollbackPatchTestCase.java
+++ src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/RollbackPatchTestCase.java
@@ -1,15 +1,19 @@
 package org.jboss.as.console.testsuite.tests.runtime.patching;
 
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.console.testsuite.fragments.shared.modals.WizardWindow;
+import org.jboss.as.console.testsuite.fragments.config.patching.PatchingWizard;
 import org.jboss.as.console.testsuite.pages.runtime.PatchManagementPage;
+import org.jboss.as.console.testsuite.tests.categories.StandaloneTest;
 import org.jboss.as.console.testsuite.util.Console;
+import org.jboss.qa.management.cli.PatchManager;
 import org.jboss.qa.management.common.ServerUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +22,8 @@ import org.slf4j.LoggerFactory;
  * @author rhatlapa (rhatlapa@redhat.com)
  */
 @RunWith(Arquillian.class)
+@RunAsClient
+@Category(StandaloneTest.class)
 public class RollbackPatchTestCase extends PatchTestCaseAbstract{
 
     private static final Logger log = LoggerFactory.getLogger(RollbackPatchTestCase.class);
@@ -39,26 +45,59 @@ public class RollbackPatchTestCase extends PatchTestCaseAbstract{
         if (!ServerUtils.isServerRunning(cliClient)) {
             ServerUtils.waitForServerToBecomeAvailable(cliClient);
         }
+        if (patchCliManager.isPatchInstaled(CUMULATIVE_PATCH_NAME)) {
+            removePatchViaCliUsingRollback(cliClient, CUMULATIVE_PATCH_NAME);
+        }
         removePatchViaCliUsingRollback(cliClient, BASIC_PATCH_NAME);
 
         browser.navigate().refresh();
     }
 
     @Test
-    public void rollbackPatch() {
-            WizardWindow rollbackPatchWizard = patchManagementPage.rollbackPatch(BASIC_PATCH_NAME);
-            log.debug("Started rollback wizard");
-            rollbackPatchWizard.next();
-            rollbackPatchWizard.next();
-            log.debug("Finishing rollback wizard");
+    public void rollbackCumulativePatchTest() {
+        prepareAndApplyPatchViaCli(cliClient, cumulativePatchFile, CUMULATIVE_PATCH_NAME);
+        rollbackPatch(CUMULATIVE_PATCH_NAME, true, true);
+        Assert.assertFalse("Patch rollback failed",
+                patchCliManager.getCumulativePatchId().equals(CUMULATIVE_PATCH_NAME));
+        log.debug("Patch history cum: {}", patchCliManager.getHistory().contains(new PatchManager.PatchHistoryEntry(CUMULATIVE_PATCH_NAME, "cumulative", null)));
+        Assert.assertTrue("Patch rollback failed, only cumulative patch should be rolled backed",
+                patchCliManager.getCumulativePatchId().equals(BASIC_PATCH_NAME));
+    }
+
+    @Test
+    public void rollbackOriginalAfterCumulativePatchFailureTest() {
+        prepareAndApplyPatchViaCli(cliClient, cumulativePatchFile, CUMULATIVE_PATCH_NAME);
+        rollbackPatch(BASIC_PATCH_NAME, false, true);
+        Assert.assertTrue("Patch rollback should fail => cumulative patch should still be there",
+                patchCliManager.getPatchInfo().getCumulativePatchId().equals(CUMULATIVE_PATCH_NAME));
+        log.debug("Patch history: {}", patchCliManager.getHistory().contains(new PatchManager.PatchHistoryEntry(BASIC_PATCH_NAME, "cumulative", null)));
+        Assert.assertTrue("Original patch shouldn't be rollbacked",
+                patchCliManager.getCumulativePatchId().equals(CUMULATIVE_PATCH_NAME));
+    }
+
+    @Test
+    public void rollbackPatchTest() {
+        rollbackPatch(BASIC_PATCH_NAME, true, true);
+        Assert.assertFalse("Patch rollback failed",
+                patchCliManager.getCumulativePatchId().equals(BASIC_PATCH_NAME));
+        Assert.assertFalse("Restart is required by the server, should be already done by console",
+                cliClient.restartRequired());
+    }
+
+    private void rollbackPatch(String patchName, boolean expectedResult, boolean withServerRestart) {
+        PatchingWizard rollbackPatchWizard = patchManagementPage.rollbackPatch(patchName);
+        rollbackPatchWizard.next();
+        rollbackPatchWizard.next();
+        Assert.assertEquals("Result of applying patch, " + rollbackPatchWizard.getResultMessage(), expectedResult, rollbackPatchWizard.isSuccess());
+        if (expectedResult) {
+            rollbackPatchWizard.restartSever(withServerRestart);
             rollbackPatchWizard.finish();
-            log.debug("Wizard finished");
-            ServerUtils.waitForServerToBecomeAvailable(cliClient);
-            log.debug("Server should be started");
-            Assert.assertFalse("Patch rollback failed",
-                    patchCliManager.getPatchInfo().getCumulativePatchId().equals(BASIC_PATCH_NAME));
-            Assert.assertFalse("Restart is required by the server, should be already done by console",
-                    cliClient.restartRequired());
+        } else {
+            rollbackPatchWizard.cancel();
+        }
+        assertRestartDoneByConsole(withServerRestart);
+        Assert.assertEquals("Patch rollback result", expectedResult,
+                !patchCliManager.getHistory().contains(new PatchManager.PatchHistoryEntry(patchName, null, null)));
     }
 
 }
