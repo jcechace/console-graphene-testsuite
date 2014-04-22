4e273b749491e893d9b85e643adbf88bb8ccc349
mvelas <mvelas@redhat.com>
2014-04-22 13:27:10 +0200

    Restart after patching abstraction + test

---
+++ src/test/java/org/jboss/as/console/testsuite/tests/runtime/patching/RestartAfterPatchTestCase.java
@@ -0,0 +1,63 @@
+package org.jboss.as.console.testsuite.tests.runtime.patching;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.console.testsuite.pages.runtime.PatchManagementPage;
+import org.jboss.as.console.testsuite.tests.categories.StandaloneTest;
+import org.jboss.as.console.testsuite.util.Console;
+import org.jboss.qa.management.cli.PatchManager;
+import org.jboss.qa.management.common.ServerUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by mvelas on 22.4.2014.
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+@Category(StandaloneTest.class)
+public class RestartAfterPatchTestCase extends PatchTestCaseAbstract {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplyPatchTestCase.class);
+
+    @Before
+    public void before() {
+        if (!ServerUtils.isServerRunning(cliClient)) {
+            ServerUtils.waitForServerToBecomeAvailable(cliClient);
+        }
+
+        Graphene.goTo(PatchManagementPage.class);
+        Console.withBrowser(browser).waitUntilLoaded();
+
+        patchCliManager = new PatchManager(cliClient, false);
+    }
+
+    @After
+    public void after() {
+        if (patchCliManager.isPatchInstalled(BASIC_PATCH_NAME)) {
+            removePatchViaCliUsingRollback(BASIC_PATCH_NAME);
+        }
+    }
+
+    @Test
+    public void applyPatchWithManualRestart() {
+
+        prepareAndApplyPatchViaCli(basicPatchFile, BASIC_PATCH_NAME);
+        Assert.assertTrue("Missing information that the restart is required",
+                patchManagementPage.getInfoTable().get("Latest Applied Patch").contains("Pending restart"));
+        patchManagementPage.restartServer().confirm();
+
+        ServerUtils.waitForServerToBecomeAvailable(cliClient);
+        browser.navigate().refresh();
+
+        Assert.assertFalse("Restart was performed but pending is still displayed",
+                patchManagementPage.getInfoTable().get("Latest Applied Patch").contains("Pending restart"));
+    }
+}
